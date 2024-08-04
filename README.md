174f1a0cbb147513e67de9cec632128a2fb5bb70
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2024-08-05 00:50:40 +0530

    structure  improvement

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # New_Backend_cont
-this is a simple but complex in term of functiomnallity , this is a youtube like project
---
+++ src/constant.js
@@ -0,0 +1,2 @@
+// this is used to store constant variable like exporting of data and other stuffs
+export const DB_NAME ="TubeMaster"
