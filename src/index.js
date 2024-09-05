2253aa2e3134195406a5b8a4d567a67932b93e0e
DrDead0 <112349103+DrDead0@users.noreply.github.com>
2024-09-06 00:58:38 +0530

    Database Connection

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import mongoose from "mongoose";
 import { DB_NAME } from "./constant.js";
 import connectDB from "./db/index.js";
 import { config } from "dotenv";
-
+//!note always use .js while importing any file which you made 
 dotenv.config({
     path:'./env'
 })
